7360eadb34a52dd983fb7cc0be412460d7f7884e
Salwa MECHERGUI <salwa@craftfoundry.tech>
2020-04-17 14:26:40 +0100

    code12345

--- controller/authentification.go
+++ controller/authentification.go
@@ -2,24 +2,22 @@ package controller
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 //endpoint creation
-// func CreateEndpoint(w http.ResponseWriter, r *http.Request) {
-
-// 	response, err := http.Post(os.Getenv("URL"), "application/json", r.Body)
-// 	if err != nil {
-// 		fmt.Printf("The HTTP request failed with error %s\n", err)
-// 	} else {
-// 		data, _ := ioutil.ReadAll(response.Body)
-// 		w.WriteHeader(response.StatusCode)
-// 		w.Write(data)
-// 		fmt.Fprintf(w, "Hello world")
-// 		return
-// 	}
-
-// }
 func CreateEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world")
+
+	response, err := http.Post(os.Getenv("URL"), "application/json", r.Body)
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		w.WriteHeader(response.StatusCode)
+		w.Write(data)
+		return
+	}
+
 }
